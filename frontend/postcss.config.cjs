2c7d8fcd3d543dc80003451dbf54fd1db98b873d
maruf-ahmed192 <19203034@iubat.edu>
2022-10-13 04:37:04 +0600

    Add dark mode and charts js

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        cssnano: {
+            preset: "default",
+        },
+    },
 }
