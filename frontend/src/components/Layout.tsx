9c20e38e5d50dd6ab18e3feb764ad33313519519
maruf-ahmed192 <19203034@iubat.edu>
2022-10-13 10:42:45 +0600

    Add theme toggle option

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,7 +2,37 @@ import React from "react"
 import DesktopSidebar from "./DesktopSidebar"
 import MobileSidebar from "./MobileSidebar"
 
+const setTheme = (theme: string) => {
+    localStorage.setItem("theme", theme)
+    document.documentElement.className = theme
+}
+
 function Layout({ children }: { children: React.ReactNode }) {
+    const [isDarkMode, setIsDarkMode] = React.useState(false)
+
+    React.useEffect(() => {
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (
+            window.localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+            setTheme("dark")
+        } else {
+            setTheme("light")
+        }
+        // Whenever the user explicitly chooses to respect the OS preference
+        // window.localStorage.removeItem("theme")
+    }, [])
+    const toggleDarkMode = () => {
+        if (window.localStorage.theme === "dark") {
+            setTheme("light")
+            setIsDarkMode(false)
+        } else {
+            setTheme("dark")
+            setIsDarkMode(true)
+        }
+    }
     return (
         <div
             className="flex h-screen bg-gray-50 dark:bg-gray-900"
@@ -62,29 +92,32 @@ function Layout({ children }: { children: React.ReactNode }) {
                             <li className="flex">
                                 <button
                                     className="rounded-md focus:outline-none focus:shadow-outline-purple"
-                                    //   @click="toggleTheme"
+                                    onClick={toggleDarkMode}
                                     aria-label="Toggle color mode"
                                 >
-                                    {/* <svg
+                                    {isDarkMode ? (
+                                        <svg
                                             className="w-5 h-5"
                                             aria-hidden="true"
                                             fill="currentColor"
                                             viewBox="0 0 20 20"
                                         >
                                             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                                        </svg> */}
-                                    <svg
-                                        className="w-5 h-5"
-                                        aria-hidden="true"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                            clip-rule="evenodd"
-                                        ></path>
-                                    </svg>
+                                        </svg>
+                                    ) : (
+                                        <svg
+                                            className="w-5 h-5"
+                                            aria-hidden="true"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                                clip-rule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    )}
                                 </button>
                             </li>
                             {/* <!-- Notifications menu --> */}
