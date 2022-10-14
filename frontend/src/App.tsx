9526e0fc947444c8d878d5be1591e1317c53278f
maruf-ahmed192 <19203034@iubat.edu>
2022-10-15 02:54:59 +0600

    Add 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,13 @@ import {
     Route,
 } from "react-router-dom"
 import Layout from "./components/Layout"
+import NotFound from "./Pages/404"
 import Forms from "./Pages/Forms"
 import Home from "./Pages/Home"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />} errorElement={<NotFound />}>
             <Route index element={<Home />} />
             <Route path="/forms" element={<Forms />} />
         </Route>
