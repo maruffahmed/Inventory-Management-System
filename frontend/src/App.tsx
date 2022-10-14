c7fc0fa20fd1ff028ee9a57164fc48ea448586db
maruf-ahmed192 <19203034@iubat.edu>
2022-10-15 03:20:33 +0600

    Add new pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,29 @@ import {
 } from "react-router-dom"
 import Layout from "./components/Layout"
 import NotFound from "./Pages/404"
+import Buttons from "./Pages/Buttons"
+import Cards from "./Pages/Cards"
+import CreateAccount from "./Pages/CreateAccount"
+import ForgotPassword from "./Pages/ForgotPassword"
 import Forms from "./Pages/Forms"
 import Home from "./Pages/Home"
+import Login from "./Pages/Login"
+import Tables from "./Pages/Tables"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<Layout />} errorElement={<NotFound />}>
-            <Route index element={<Home />} />
-            <Route path="/forms" element={<Forms />} />
-        </Route>
+        <>
+            <Route path="/" element={<Layout />} errorElement={<NotFound />}>
+                <Route index element={<Home />} />
+                <Route path="/forms" element={<Forms />} />
+                <Route path="/cards" element={<Cards />} />
+                <Route path="/buttons" element={<Buttons />} />
+                <Route path="/tables" element={<Tables />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+        </>
     )
 )
 
