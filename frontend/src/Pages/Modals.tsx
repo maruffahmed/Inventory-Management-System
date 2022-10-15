0325b36b39586948959fa568ce95542c3086af44
maruf-ahmed192 <19203034@iubat.edu>
2022-10-16 01:05:32 +0600

    Add modal animation

--- frontend/src/Pages/Modals.tsx
+++ frontend/src/Pages/Modals.tsx
@@ -1,5 +1,5 @@
-import { Dialog } from "@headlessui/react"
-import React from "react"
+import { Dialog, Transition } from "@headlessui/react"
+import React, { Fragment } from "react"
 
 function Modals() {
     let [isOpen, setIsOpen] = React.useState(false)
@@ -54,66 +54,78 @@ function Modals() {
                         Open Modal
                     </button>
                 </div>
-                <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-                    <Dialog.Panel>
-                        <div className="fixed inset-0 z-30 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center">
-                            {/* <!-- Modal --> */}
-                            <div
-                                onClick={() => setIsOpen(false)}
-                                className="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl"
-                                role="dialog"
-                                id="modal"
-                            >
-                                {/* <!-- Remove header if you don't want a close icon. Use modal body to place modal tile. --> */}
-                                <header className="flex justify-end">
-                                    <button
-                                        className="inline-flex items-center justify-center w-6 h-6 text-gray-400 transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700"
-                                        aria-label="close"
-                                        // @click="closeModal"
-                                    >
-                                        <svg
-                                            className="w-4 h-4"
-                                            fill="currentColor"
-                                            viewBox="0 0 20 20"
-                                            role="img"
-                                            aria-hidden="true"
+                <Transition
+                    show={isOpen}
+                    enter="transition ease-out duration-150"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    as={Fragment}
+                >
+                    <Dialog onClose={() => setIsOpen(false)}>
+                        <Dialog.Panel>
+                            <div className="fixed inset-0 z-30 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center">
+                                {/* <!-- Modal --> */}
+                                <div
+                                    onClick={() => setIsOpen(false)}
+                                    className="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl"
+                                    role="dialog"
+                                    id="modal"
+                                >
+                                    {/* <!-- Remove header if you don't want a close icon. Use modal body to place modal tile. --> */}
+                                    <header className="flex justify-end">
+                                        <button
+                                            className="inline-flex items-center justify-center w-6 h-6 text-gray-400 transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700"
+                                            aria-label="close"
+                                            // @click="closeModal"
                                         >
-                                            <path
-                                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"
-                                                fill-rule="evenodd"
-                                            ></path>
-                                        </svg>
-                                    </button>
-                                </header>
-                                {/* <!-- Modal body --> */}
-                                <div className="mt-4 mb-6">
-                                    {/* <!-- Modal title --> */}
-                                    <p className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300">
-                                        Modal header
-                                    </p>
-                                    {/* <!-- Modal description --> */}
-                                    <p className="text-sm text-gray-700 dark:text-gray-400">
-                                        Lorem, ipsum dolor sit amet consectetur
-                                        adipisicing elit. Nostrum et eligendi
-                                        repudiandae voluptatem tempore!
-                                    </p>
+                                            <svg
+                                                className="w-4 h-4"
+                                                fill="currentColor"
+                                                viewBox="0 0 20 20"
+                                                role="img"
+                                                aria-hidden="true"
+                                            >
+                                                <path
+                                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd"
+                                                    fill-rule="evenodd"
+                                                ></path>
+                                            </svg>
+                                        </button>
+                                    </header>
+                                    {/* <!-- Modal body --> */}
+                                    <div className="mt-4 mb-6">
+                                        {/* <!-- Modal title --> */}
+                                        <p className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300">
+                                            Modal header
+                                        </p>
+                                        {/* <!-- Modal description --> */}
+                                        <p className="text-sm text-gray-700 dark:text-gray-400">
+                                            Lorem, ipsum dolor sit amet
+                                            consectetur adipisicing elit.
+                                            Nostrum et eligendi repudiandae
+                                            voluptatem tempore!
+                                        </p>
+                                    </div>
+                                    <footer className="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800">
+                                        <button
+                                            onClick={() => setIsOpen(false)}
+                                            className="w-full px-5 py-3 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
+                                        >
+                                            Cancel
+                                        </button>
+                                        <button className="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                                            Accept
+                                        </button>
+                                    </footer>
                                 </div>
-                                <footer className="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800">
-                                    <button
-                                        onClick={() => setIsOpen(false)}
-                                        className="w-full px-5 py-3 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
-                                    >
-                                        Cancel
-                                    </button>
-                                    <button className="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                                        Accept
-                                    </button>
-                                </footer>
                             </div>
-                        </div>
-                    </Dialog.Panel>
-                </Dialog>
+                        </Dialog.Panel>
+                    </Dialog>
+                </Transition>
             </div>
         </main>
     )
