6aea58d181458271ac8b2679ea5ca1c125ca5f23
maruffahmed <marufah76@gmail.com>
2022-11-28 00:37:36 +0600

    Clean remix app

--- frontend_remix/app/root.tsx
+++ frontend_remix/app/root.tsx
@@ -1,45 +1,32 @@
-import type { MetaFunction, LinksFunction } from "@remix-run/node"
+import type { MetaFunction } from "@remix-run/node";
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-} from "@remix-run/react"
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 
-import styles from "./styles/app.css"
-import Layout from "./components/Layout"
-
-export const links: LinksFunction = () => {
-    return [{ rel: "stylesheet", href: styles }]
-}
 export const meta: MetaFunction = () => ({
-    charset: "utf-8",
-    title: "New Remix App",
-    viewport: "width=device-width,initial-scale=1",
-})
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
 
 export default function App() {
-    return (
-        <Document>
-            {/* <Layout> */}
-            <Outlet />
-            <ScrollRestoration />
-            <Scripts />
-            <LiveReload />
-            {/* </Layout> */}
-        </Document>
-    )
-}
-function Document({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <head>
-                <Meta />
-                <Links />
-            </head>
-            <body>{children}</body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
