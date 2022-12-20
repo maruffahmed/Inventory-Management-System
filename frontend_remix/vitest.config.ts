4cca6cb6d6414e0037a3b31ca638dc63e6d28212
maruffahmed <marufah76@gmail.com>
2022-12-21 03:07:20 +0600

    Add test coverage

--- frontend_remix/vitest.config.ts
+++ frontend_remix/vitest.config.ts
@@ -8,6 +8,10 @@ import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     test: {
+        coverage: {
+            provider: "c8",
+            reporter: ["text", "json", "html"],
+        },
         globals: true,
         environment: "happy-dom",
         setupFiles: ["./test/setup-test-env.ts"],
