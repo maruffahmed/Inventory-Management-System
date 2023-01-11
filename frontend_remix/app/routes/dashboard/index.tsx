0a8b7cb8e1e052833f43e28c125867cc99859f6b
maruffahmed <marufah76@gmail.com>
2023-01-12 03:40:10 +0600

    Test change on dashboard

--- frontend_remix/app/routes/dashboard/index.tsx
+++ frontend_remix/app/routes/dashboard/index.tsx
@@ -178,7 +178,7 @@ function Index() {
         <main className="h-full overflow-y-auto">
             <div className="container px-6 mx-auto grid">
                 <h2 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-                    Dashboard
+                    Home Dashboard
                 </h2>
                 {/* <!-- Cards --> */}
 
